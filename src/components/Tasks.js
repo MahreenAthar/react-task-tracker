51a2542f3cb0e7fd43735cfa91f37384b1385773
MahreenAthar <mahreenathar.oct10@gmail.com>
2021-08-27 00:09:04 +0500

    Made Task.js in /component and modified tasks

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,28 +1,9 @@
-const Tasks = () => {
-    const [tasks, setTasks] = useState([
-    {
-        id: 1,
-        text: 'Doctors Appointment',
-        day: 'Feb 5th at 2:30 pm',
-        reminder: true,
-    },
-    {
-        id: 2,
-        text: 'Meeting at school',
-        day: 'Feb 6th at 1:30 pm',
-        reminder: true,
-    },
-    {
-        id: 3,
-        text: 'Food shopping',
-        day: 'Feb 5th at 2:30 pm',
-        reminder: false,
-    }
-])
+import Task from "./Task"
 
+const Tasks = ({ tasks }) => {
     return (
         <>
-          {tasks.map((task) => (<h3 key={task.id}>{task.text}</h3>))}  
+          {tasks.map((task) => (<Task key={task.id} task={task}/> ))}
         </>
     )
 }
