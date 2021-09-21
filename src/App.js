cd796b2ee33d4de0c19e73649d7f7e7906669633
MahreenAthar <mahreenathar.oct10@gmail.com>
2021-09-21 14:11:06 +0500

    Add task connected with server

--- src/App.js
+++ src/App.js
@@ -28,15 +28,30 @@ function App() {
     }
 
   // Add Task
-  const addTask = (task) => {
-    const id = Math.floor(Math.random() * 10000) + 1
-    const newTask = { id, ...task }
-    setTasks([...tasks, newTask])
+  const addTask = async (task) => {
+    const res = await fetch('http://localhost:5000/tasks', {
+      method: 'POST', 
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(task)   // turns js object into a json string
+    })
+
+    const data = await res.json()
+
+    setTasks([...tasks, data])
+
+
+    // const id = Math.floor(Math.random() * 10000) + 1
+    // const newTask = { id, ...task }
+    // setTasks([...tasks, newTask])
   }
 
   // Delete Task
   const deleteTask = async (id) => {
-    await fetch(`http://localhost:5000/tasks/${id}`, )
+    await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'DELETE'
+    })
 
     setTasks(tasks.filter((task) => task.id !== id))
   }
