b066881f2fe1670436da12228500ff6788dcc74f
MahreenAthar <mahreenathar.oct10@gmail.com>
2021-09-24 13:27:06 +0500

    Connecter reminder toggle with server

--- src/App.js
+++ src/App.js
@@ -69,11 +69,21 @@ function App() {
   // Toggle Reminder
   const toggleReminder = async (id) => {
     const taskToToggle = await fetchTask(id)
-    const
+    const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder }
+
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-type' : 'application/json'
+      },
+      body: JSON.stringify(updTask)
+    })
+
+    const data = await res.json()
 
     setTasks(
       tasks.map((task) =>
-        task.id === id ? { ...task, reminder: !task.reminder } : task)
+        task.id === id ? { ...task, reminder: data.reminder } : task)
     )
   }
 
